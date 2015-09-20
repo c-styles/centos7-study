696ffb946b43cf0e94d98c21ef1cd1fad558da23
Eisuke Kozu <eisuke@c-styles.jp>
2015-09-20 16:39:29 +0900

    berks init済
    ただし現時点で要らない機能はコメントアウトか削除した

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'centos7-study'
+maintainer       'c-styles'
+maintainer_email 'eisuke@c-styles.jp'
+license          'All rights reserved'
+description      'Learning Material for CentOS7'
+long_description ''
+version          '0.1.0'
+
+depends          'git'
